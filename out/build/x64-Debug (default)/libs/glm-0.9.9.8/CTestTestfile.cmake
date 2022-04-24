c9c174508a14ca12a0d8620026e25c25cf26d4ae
Collin Brown <CJBrown7966@gmail.com>
2022-04-23 18:42:06 -0700

    Untracked Build Folder

--- out/build/x64-Debug (default)/libs/glm-0.9.9.8/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/CJBro/Desktop/Coding/RPGs/The Moonlight Tongue/libs/glm-0.9.9.8
-# Build directory: C:/Users/CJBro/Desktop/Coding/RPGs/The Moonlight Tongue/out/build/x64-Debug (default)/libs/glm-0.9.9.8
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- out/build/x64-Debug (default)/libs/glm-0.9.9.8/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/CJBro/Desktop/Coding/RPGs/The Moonlight Tongue/libs/glm-0.9.9.8/glm
-# Build directory: C:/Users/CJBro/Desktop/Coding/RPGs/The Moonlight Tongue/out/build/x64-Debug (default)/libs/glm-0.9.9.8/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
